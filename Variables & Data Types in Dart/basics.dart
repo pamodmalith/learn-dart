63331c8206959c3c624961a7b3cb2216cdfef5b4
Pamod Malith <pamodmalith@gmail.com>
2025-03-11 22:13:51 +0530

    variables

---
+++ Variables & Data Types in Dart/basics.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 25; // Integer
+  double pi = 3.14; // Decimal number
+  String name = "Flutter Dev"; // Text
+  bool isLearning = true; // True or False
+
+  print(age);
+  print(pi);
+  print(name);
+  print(isLearning);
+}
