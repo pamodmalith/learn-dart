9f13fefab90e70433dae4dea968bf2856c348f57
Pamod Malith <pamodmalith@gmail.com>
2025-03-11 22:20:26 +0530

    flow controllers

---
+++ control flows/do while.dart
@@ -0,0 +1,8 @@
+void main() {
+  int num = 6;
+
+  do {
+    print("Number is: $num");
+    num++;
+  } while (num <= 5);
+}
---
+++ control flows/if.dart
@@ -0,0 +1,11 @@
+void main() {
+  int score = 85;
+
+  if (score >= 75) {
+    print("Grade: A");
+  } else if (score >= 75) {
+    print("Grade: B");
+  } else {
+    print("Grade: C");
+  }
+}
