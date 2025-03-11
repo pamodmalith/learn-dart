c114ea230e0dbee9350d302a580f3651223ef246
Pamod Malith <pamodmalith@gmail.com>
2025-03-11 22:17:24 +0530

    functions

---
+++ Functions/short syntax.dart
@@ -0,0 +1,5 @@
+int multiply(int a, int b) => a * b;
+
+void main() {
+  print(multiply(4, 5)); // Output: 20
+}
---
+++ Functions/withParameter.dart
@@ -0,0 +1,8 @@
+void greet(String name) {
+  print("Hello, $name!");
+}
+
+void main() {
+  greet("Alex"); // Output: Hello, Alex!
+  greet("John"); // Output: Hello, John!
+}
